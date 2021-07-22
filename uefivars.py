awslabs/python-uefivars

commit 34cee3b1fa8a66695156a8d70d62e5fae3c700f3

Add initial uefivars code
UEFI variable stores come in different binary formats. This python module
implements readers and writes for different formats and allows conversion
between them.

With the initial version, we can read the EDK2 one and read and write the
AWS as well as a self built JSON representation.

Signed-off-by: Alexander Graf <graf@amazon.com>

uefivars.py
@@ -1,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+import argparse
+import sys
+from uefivars import *
+
+def Str2UEFIVarStore(s):
+    if s == 'aws':
+        return AWSUEFIVarStore
+    elif s == 'edk2':
+        return EDK2UEFIVarStore
+    elif s == 'json':
+        return JSONUEFIVarStore
+    else:
+        raise('Unknown Input type "%s", choose from ("aws, "json")' % s)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help='Input type ("aws", "json")', required=True)
+parser.add_argument("-o", "--output", help='Output type ("aws", "json")', required=True)
+parser.add_argument("-I", "--inputfile", help='Input file (stdin if not given)')
+parser.add_argument("-O", "--outputfile", help='Output file (stdout if not given)')
+args = parser.parse_args()
+
+if args.inputfile:
+    infile = open(args.inputfile, "rb")
+else:
+    infile = sys.stdin()
+indata = infile.read()
+
+inclass = Str2UEFIVarStore(args.input)
+outclass = Str2UEFIVarStore(args.output)
+
+varstore = inclass(indata)
+varstore.__class__ = outclass
+
+if args.outputfile:
+    outfile = open(args.outputfile, "wb")
+else:
+    outfile = sys.stdout.buffer
+outfile.write(bytes(varstore))

.gitignore
@@ -1,0 +1,19 @@
+*~
+*#
+*.swp
+
+*.DS_Store
+
+__pycache__/
+*.py[cod]
+*$py.class
+*.egg-info/
+
+/.coverage
+/.coverage.*
+/.cache
+/.pytest_cache
+/.mypy_cache
+
+/doc/_apidoc/
+/build

LICENSE
@@ -1,0 +1,21 @@
+MIT License
+
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.

setup.py
@@ -1,0 +1,26 @@
+#!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uefivars",
+    version="0.0.1",
+    author="Alexander Graf",
+    author_email="graf@amazon.com",
+    description="UEFI variable store tools",
+    long_description=long_description,
+    url="https://github.com/agraf/python-uefivars",
+    packages=setuptools.find_packages(),
+    install_requires=[ 'crc32c', 'llfuse' ]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

uefivars/__init__.py
@@ -1,0 +1,7 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+from .varstore import *
+from .aws import AWSUEFIVarStore
+from .edk2 import EDK2UEFIVarStore
+from .json import JSONUEFIVarStore

uefivars/aws.py
@@ -1,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+import json
+import os
+import zlib
+import base64
+import tempfile
+import crc32c
+from .varstore import *
+from .aws_v0 import *
+from .aws_file import *
+
+class AWSUEFIVarStore(UEFIVarStore):
+    EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS = 0x20
+    AMZNUEFI = 0x494645554e5a4d41
+
+    def __init__(self, b64data: bytes):
+        super().__init__()
+
+        # Convert base64 to binary
+        file = tempfile.SpooledTemporaryFile()
+        file.write(base64.b64decode(b64data))
+        file.seek(0, os.SEEK_SET)
+
+        # Then wrap the binary file with our reader and start parsing
+        file = AWSVarStoreFile(file)
+        magic = file.read64()
+        if magic != self.AMZNUEFI:
+            raise Exception("Invalid magic. Expected AMZNUEFI. Found 0x%x" % magic);
+        crc32 = file.read32()
+
+        # Validate crc32c
+        location = file.file.tell()
+        comp_crc32 = crc32c.crc32(file.readall())
+        if (comp_crc32 != crc32):
+            raise Exception("Invalid checksum, please check you copied all data")
+        file.file.seek(location, os.SEEK_SET)
+
+        version = file.read32()
+        if version != 0:
+            raise Exception("Invalid version. Expected 0. Found 0x%x" % version);
+
+        # Grab the zlib data that's embedded and parse it
+        dec = zlib.decompressobj(0, zdict=UEFIVarStoreV0.dict)
+        raw_data = dec.decompress(file.readall())
+        raw_file = tempfile.SpooledTemporaryFile()
+        raw_file.write(raw_data)
+        raw_file.seek(0, os.SEEK_SET)
+        raw = AWSVarStoreFile(raw_file)
+        nr_entries = raw.read64()
+        for i in range(nr_entries):
+            name = raw.readstr()
+            data = raw.readdata()
+            guid = raw.readguid()
+            attr = raw.read32()
+            if attr & self.EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS:
+                timestamp = raw.readtimestamp()
+                digest = raw.readdata()
+                self.vars.append(UEFIVar(name, data, guid, attr, timestamp, digest))
+            else:
+                self.vars.append(UEFIVar(name, data, guid, attr))
+
+    def __bytes__(self) -> bytes:
+        # Assemble the zlib compressed wrapped file
+        raw = AWSVarStoreFile(tempfile.SpooledTemporaryFile())
+        raw.write64(len(self.vars))
+        for var in self.vars:
+            raw.writestr(var.name)
+            raw.writedata(var.data)
+            raw.writeguid(var.guid)
+            raw.write32(var.attr)
+            if var.attr & self.EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS:
+                raw.writetimestamp(var.timestamp)
+                raw.writedata(var.digest)
+        raw.file.seek(0, os.SEEK_SET)
+
+        enc = zlib.compressobj(9, zdict=UEFIVarStoreV0.dict)
+        zdata = enc.compress(raw.file.read()) + enc.flush()
+
+        # Create a full file with header + zdata
+        f = tempfile.SpooledTemporaryFile()
+        f = AWSVarStoreFile(f)
+        f.write64(self.AMZNUEFI)
+        f.write32(crc32c.crc32(int(0).to_bytes(4, byteorder='little') + zdata))
+        f.write32(0) # Version 0
+        f.write(zdata)
+        f.file.seek(0, os.SEEK_SET)
+
+        # Then write it out as base64 data
+        return base64.b64encode(f.file.read())
+
+    def __str__(self) -> str:
+        return self.__bytes__().decode('utf-8')

uefivars/aws_file.py
@@ -1,0 +1,70 @@
+#!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+
+class AWSVarStoreFile(object):
+    def __init__(self, file):
+        self.file = file
+
+    def read(self, size):
+        value = self.file.read(size)
+        if len(value) != size:
+            raise Exception("Unexpected end of %s at 0x%x" % (self.file, self.file.tell()))
+        return value
+
+    def read64(self):
+        return int.from_bytes(self.read(8), byteorder='little', signed=False)
+
+    def read32(self):
+        return int.from_bytes(self.read(4), byteorder='little', signed=False)
+
+    def read16(self):
+        return int.from_bytes(self.read(2), byteorder='little', signed=False)
+
+    def read8(self):
+        return int.from_bytes(self.read(1), byteorder='little', signed=False)
+
+    def readdata(self):
+        size = self.read64()
+        return self.read(size)
+
+    def readstr(self):
+        return self.readdata().decode('utf-8')
+
+    def readguid(self):
+        return self.read(16)
+
+    def readtimestamp(self):
+        return self.read(16)
+
+    def readall(self):
+        return self.file.read()
+
+    def write(self, data):
+        self.file.write(data)
+
+    def write64(self, data):
+        self.write(data.to_bytes(8, byteorder='little'))
+
+    def write32(self, data):
+        self.write(data.to_bytes(4, byteorder='little'))
+
+    def write16(self, data):
+        self.write(data.to_bytes(2, byteorder='little'))
+
+    def write8(self, data):
+        self.write(data.to_bytes(1, byteorder='little'))
+
+    def writedata(self, data):
+        self.write64(len(data))
+        self.write(data)
+
+    def writestr(self, data):
+        return self.writedata(data.encode('utf-8'))
+
+    def writeguid(self, data):
+        return self.write(data)
+
+    def writetimestamp(self, data):
+        return self.write(data)

commit 7a34b5131dd5c0bb56e7db6c8eb974c4c77bccca

uefivars: Fix stdin fallback
We incorrectly treated sys.stdin as a function instead of an object.
Fix empty -I by using it as a file object.

Signed-off-by: Alexander Graf <graf@amazon.com>
Fixes: 34cee3b1fa8 ("Add initial uefivars code")

uefivars.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 if args.inputfile:
     infile = open(args.inputfile, "rb")
 else:
-    infile = sys.stdin()
+    infile = sys.stdin
 indata = infile.read()
 
 inclass = Str2UEFIVarStore(args.input)