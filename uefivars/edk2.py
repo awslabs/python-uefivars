8a950326ffc12530889b88f256f68d7d59f79c54
Alexander Graf <graf@amazon.com>
2021-07-22 16:21:07 +0200

    edk2: Parse certdb
    The edk2 variable store puts digests into a separate variable rather than
    attach them to the variable record.
    
    Add code to interpret the certdb format and populate digest properties
    from it.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>

--- uefivars/edk2.py
+++ uefivars/edk2.py
@@ -8,9 +8,47 @@ import os
 from .aws_file import *
 from .varstore import *
 
+class EDK2Cert(object):
+    def __init__(self, name: str, digest: bytes):
+        self.name = name
+        self.digest = digest
+
+class EDK2CertDB(object):
+    def __init__(self, uefivar: UEFIVar = None):
+        self.certs = []
+        if uefivar != None:
+            self.init_from_var(uefivar)
+
+    def init_from_var(self, uefivar: UEFIVar):
+        file = tempfile.SpooledTemporaryFile()
+        file.write(uefivar.data)
+        file.seek(0, os.SEEK_SET)
+        file = AWSVarStoreFile(file)
+        size = file.read32()
+        if size != len(uefivar.data):
+            raise Exception("Invalid certdb length")
+        size = size - 4
+
+        while size != 0:
+            vendor_guid = file.readguid()
+            cert_node_size = file.read32()
+            name_size = file.read32() * 2
+            digest_size = file.read32()
+            name = file.read(name_size).decode('utf-16le').rstrip('\0')
+            digest = file.read(digest_size)
+            self.certs.append(EDK2Cert(name, digest))
+            size = size - (16 + 4 + 4 + 4 + name_size + digest_size)
+
 class EDK2UEFIVarStore(UEFIVarStore):
+    GUID_CERTDB = b'\x6e\xe5\xbe\xd9\xdc\x75\xd9\x49\xb4\xd7\xb5\x34\x21\x0f\x63\x7a'
+    GUID_NVFS = b'\x8d\x2b\xf1\xff\x96\x76\x8b\x4c\xa9\x85\x27\x47\x07\x5b\x4f\x50'
+    GUID_VARSTORE = b'\x78\x2c\xf3\xaa\x7b\x94\x9a\x43\xa1\x80\x2e\x14\x4e\xc3\x77\x92'
+    STATE_SETTLED = 0x3f
+
     def __init__(self, data):
         super().__init__()
+
+        self.certdb = EDK2CertDB()
 
         # Get a streaming file object
         file = tempfile.SpooledTemporaryFile()
@@ -24,7 +62,7 @@ class EDK2UEFIVarStore(UEFIVarStore):
             raise Exception("Invalid Zero Vector: %s" % zerovector)
 
         fsguid = file.readguid()
-        if fsguid != b'\x8d\x2b\xf1\xff\x96\x76\x8b\x4c\xa9\x85\x27\x47\x07\x5b\x4f\x50':
+        if fsguid != self.GUID_NVFS:
             raise Exception("Invalid GUID: %s" % fsguid)
 
         self.length = file.read64()
@@ -47,7 +85,7 @@ class EDK2UEFIVarStore(UEFIVarStore):
 
         file.read(0x10)
         vsguid = file.readguid()
-        if vsguid != b'\x78\x2c\xf3\xaa\x7b\x94\x9a\x43\xa1\x80\x2e\x14\x4e\xc3\x77\x92':
+        if vsguid != self.GUID_VARSTORE:
             raise Exception('Invalid Varstore GUID: %s' % vsguid)
 
         self.varsize = file.read32()
@@ -68,6 +106,18 @@ class EDK2UEFIVarStore(UEFIVarStore):
             guid = file.readguid()
             name = file.read(namelen).decode('utf-16le').rstrip('\0')
             data = file.read(datalen)
-            if state == 0x3f:
-                self.vars.append(UEFIVar(name, data, guid, attr, timestamp, bytes(pubkeyidx)))
+            if state == self.STATE_SETTLED:
+                if timestamp == b'\0' * 16:
+                    timestamp = None
+                var = UEFIVar(name, data, guid, attr, timestamp, None)
+                if name == "certdb" and guid == self.GUID_CERTDB:
+                    self.certdb = EDK2CertDB(var)
+                else:
+                    self.vars.append(var)
             file.file.seek((file.file.tell() + 0x3) & ~0x3, os.SEEK_SET)
+
+        # Extract all certdb entries into digest fields
+        for cert in self.certdb.certs:
+            for var in self.vars:
+                if var.name == cert.name:
+                    var.digest = cert.digest
