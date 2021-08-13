9345de6c2b6c3df879c5139943b72db3b638d98b
Alexander Graf <graf@amazon.com>
2021-08-13 09:53:28 +0200

    setup.py: Fix location
    Now that the project has an official home in the awslabs repository,
    let's declare it as such and bump its version number accordingly.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uefivars",
-    version="0.0.1",
-    author="Alexander Graf",
+    version="0.1",
+    author="Amazon Web Services",
     author_email="graf@amazon.com",
     description="UEFI variable store tools",
     long_description=long_description,
-    url="https://github.com/agraf/python-uefivars",
+    url="https://github.com/awslabs/python-uefivars",
     packages=setuptools.find_packages(),
-    install_requires=[ 'crc32c', 'llfuse' ]
+    install_requires=[ 'crc32c' ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
