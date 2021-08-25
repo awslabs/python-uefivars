1a238897a3fff7ba93d93096bf6835223c6b2932
Johanna Amélie Schander <mimoja@amazon.de>
2021-08-25 12:28:49 +0200

    Add secureboot variable support
    Allowing users to set their own secureboot variable is the main goal
    of the persistent uefi variable store. To make this process more comfortable
    we are providing direct switches to (over-)write those variables.
    
    Signed-off-by: Johanna Amélie Schander <mimoja@amazon.de>

--- uefivars.py
+++ uefivars.py
@@ -7,6 +7,9 @@ import argparse
 import sys
 from uefivars import *
 
+globalEfiGUID = bytes.fromhex("61 df e4 8b ca 93 d2 11 aa 0d 00 e0 98 03 2b 8c")
+secureDatabaseGUID = bytes.fromhex("cb b2 19 d7 3a 3d 96 45 a3 bc da d0 0e 67 65 6f")
+
 def Str2UEFIVarStore(s):
     if s == 'aws':
         return AWSUEFIVarStore
@@ -21,11 +24,38 @@ def Str2UEFIVarStore(s):
             'Unknown Input type "{}", choose from ("aws, "json", "edk2", "none")'.format(s)
         )
 
+def ReadVar(arg, name, guid):
+    EFI_VARIABLE_NON_VOLATILE=0x00000001
+    EFI_VARIABLE_BOOTSERVICE_ACCESS=0x00000002
+    EFI_VARIABLE_RUNTIME_ACCESS=0x00000004
+    EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS=0x00000020
+
+    attr = EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS | EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS
+
+    varfile = open(arg, "rb")
+    vardata = varfile.read()
+
+    if(len(vardata) == 0):
+        print('Read empty variable "{}". Aborting'.format(name), file=sys.stderr)
+        sys.exit()
+
+    return UEFIVar(name, vardata, guid, attr)
+
+pk_found = -1
+kek_found = -1
+db_found = -1
+dbx_found = -1
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help='Input type ("aws", "json", "edk2", "none")', required=True)
 parser.add_argument("-o", "--output", help='Output type ("aws", "json", "edk2")', required=True)
 parser.add_argument("-I", "--inputfile", help='Input file (stdin if not given)')
 parser.add_argument("-O", "--outputfile", help='Output file (stdout if not given)')
+parser.add_argument("-P", "--PK", help='Insert PK from given file (usually PK.esl)')
+parser.add_argument("-K", "--KEK", help='Insert KEK from given file (usually KEK.esl)')
+parser.add_argument("-b", "--db", help='Insert db from given file (usually db.esl)')
+parser.add_argument("-x", "--dbx", help='Insert dbx from given file (usually dbx.esl)')
+
 args = parser.parse_args()
 
 inclass = Str2UEFIVarStore(args.input)
@@ -43,6 +73,60 @@ else:
     indata = infile.read()
 
 varstore = inclass(indata)
+
+print("Read {} variables".format(varstore.vars.__len__()), file=sys.stderr)
+
+for i, s in enumerate(varstore.vars):
+    var = varstore.vars[i]
+
+    if (var.name == 'PK' and var.guid == globalEfiGUID):
+        pk_found = i
+
+    if (var.name == 'KEK' and var.guid == globalEfiGUID):
+        kek_found = i
+
+    if (var.name == 'db' and var.guid == secureDatabaseGUID):
+        db_found = i
+
+    if (var.name == 'dbx' and var.guid == secureDatabaseGUID):
+        dbx_found = i
+
+if (args.PK):
+    var = ReadVar(args.PK, 'PK', globalEfiGUID)
+    if (pk_found != -1):
+        print('Replacing PK', file=sys.stderr)
+        varstore.vars[pk_found] = var
+    else:
+        varstore.vars.append(var)
+elif (pk_found == -1):
+    print(pk_found, file=sys.stderr)
+    print('No PK (PlatformKey) was set; SecureBoot will not be enabled without a PK', file=sys.stderr)
+
+if (args.KEK):
+    var =  ReadVar(args.KEK, 'KEK', globalEfiGUID)
+    if (kek_found != -1):
+        print('Replacing KEK', file=sys.stderr)
+        varstore.vars[kek_found] = var
+    else:
+        varstore.vars.append(var)
+
+if (args.db):
+    var = ReadVar(args.db, 'db', secureDatabaseGUID)
+    if (db_found != -1):
+        print('Replacing db', file=sys.stderr)
+        varstore.vars[db_found] = var
+    else:
+        varstore.vars.append(var)
+
+if (args.dbx):
+    var = ReadVar(args.dbx, 'dbx', secureDatabaseGUID)
+    if (dbx_found != -1):
+        print('Replacing dbx', file=sys.stderr)
+        varstore.vars[dbx_found] = var
+    else:
+        varstore.vars.append(var)
+
+# convert the format by changing the output class
 varstore.__class__ = outclass
 
 if args.outputfile:
@@ -50,3 +134,5 @@ if args.outputfile:
 else:
     outfile = sys.stdout.buffer
 outfile.write(bytes(varstore))
+
+print("Writen {} variables".format(varstore.vars.__len__()), file=sys.stderr)
